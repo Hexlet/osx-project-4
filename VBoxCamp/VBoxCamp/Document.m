6c9fe0b97ad347000f4084e04b142a3bc5680472
Dmitry Zavorokhin <goodman116@gmail.com>
2013-02-15 01:45:53 +0400

    [f] escape characters are put into creation VM script

--- VBoxCamp/VBoxCamp/Document.m
+++ VBoxCamp/VBoxCamp/Document.m
@@ -16,8 +16,8 @@
     self = [super init];
     if (self) {
         volumes = [self volumesInfo];
-        _vm_name = @"TEST";
-        _vmdk_file_name = @"test";
+        _vm_name = @"BOOTCAMP_VM";
+        _vmdk_file_name = @"bootcampvm";
     }
     return self;
 }
@@ -134,67 +134,67 @@
         [openDlg setPrompt:NSLocalizedString(@"SELECT_BTN", "SELECT_BTN")];
         if ([openDlg runModal] == NSOKButton) {
             NSString *bootcamp_vm_dir = [[openDlg URL] path];
-//            NSString *chmod_dev = [NSString stringWithFormat:@"sudo chmod a+rw /dev/%@;", v.bsdId];
-//            NSString *cd_vm_dir = [NSString stringWithFormat:@"cd %@;", bootcamp_vm_dir];
-//            NSRange range = [v.bsdId rangeOfString:@"disk"];
-//            NSArray *disk_s = [[v.bsdId substringFromIndex:NSMaxRange(range)] componentsSeparatedByString:@"s"];
-//            NSString *vbm_create = [NSString stringWithFormat:@"sudo VBoxManage internalcommands createrawvmdk -rawdisk /dev/disk%@ -filename %@.vmdk -partitions %@;", disk_s[0], _vmdk_file_name, disk_s[1]];
-//            NSString *chmod_chown_vmdk = [NSString stringWithFormat:@"sudo chmod a+rw %@/*.vmdk; sudo chown %@ %@/*.vmdk", bootcamp_vm_dir, NSUserName(), bootcamp_vm_dir];
-//            
-//            //Assemble AppleScript
-//            NSMutableString *create_vmdk_source = [[NSMutableString alloc] init];
-//            [create_vmdk_source appendString:@"do shell script \""];
-//            [create_vmdk_source appendString:chmod_dev];
-//            [create_vmdk_source appendString:cd_vm_dir];
-//            [create_vmdk_source appendString:vbm_create];
-//            [create_vmdk_source appendString:chmod_chown_vmdk];
-//            [create_vmdk_source appendString:@"\" with administrator privileges"];
-//            
-//            //Print details to console
-//            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), chmod_dev]];
-//            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), cd_vm_dir]];
-//            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), vbm_create]];
-//            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), chmod_chown_vmdk]];
-//            
-//            //Execute AppleScript
-//            NSAppleScript *create_vmdk_script = [[NSAppleScript alloc] initWithSource:create_vmdk_source];
+            NSString *chmod_dev = [NSString stringWithFormat:@"sudo chmod a+rw /dev/%@;", v.bsdId];
+            NSString *cd_vm_dir = [NSString stringWithFormat:@"cd %@;", bootcamp_vm_dir];
+            NSRange range = [v.bsdId rangeOfString:@"disk"];
+            NSArray *disk_s = [[v.bsdId substringFromIndex:NSMaxRange(range)] componentsSeparatedByString:@"s"];
+            NSString *vbm_create = [NSString stringWithFormat:@"sudo VBoxManage internalcommands createrawvmdk -rawdisk /dev/disk%@ -filename %@.vmdk -partitions %@;", disk_s[0], _vmdk_file_name, disk_s[1]];
+            NSString *chmod_chown_vmdk = [NSString stringWithFormat:@"sudo chmod a+rw %@/*.vmdk; sudo chown %@ %@/*.vmdk", bootcamp_vm_dir, NSUserName(), bootcamp_vm_dir];
+            
+            //Assemble AppleScript
+            NSMutableString *create_vmdk_source = [[NSMutableString alloc] init];
+            [create_vmdk_source appendString:@"do shell script \""];
+            [create_vmdk_source appendString:chmod_dev];
+            [create_vmdk_source appendString:cd_vm_dir];
+            [create_vmdk_source appendString:vbm_create];
+            [create_vmdk_source appendString:chmod_chown_vmdk];
+            [create_vmdk_source appendString:@"\" with administrator privileges"];
+            
+            //Print details to console
+            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), chmod_dev]];
+            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), cd_vm_dir]];
+            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), vbm_create]];
+            [self appendTextToDetails:[NSString stringWithFormat:NSLocalizedString(@"RUNNING_SCRIPT_MSG", "RUNNING_SCRIPT_MSG"), chmod_chown_vmdk]];
+            
+            //Execute AppleScript
+            NSAppleScript *create_vmdk_script = [[NSAppleScript alloc] initWithSource:create_vmdk_source];
             NSDictionary *errorDict;
-//            [create_vmdk_script executeAndReturnError:&errorDict];
-//            [self appendTextToDetails:NSLocalizedString(@"DONE_MSG", "DONE_MSG")];
-//
-//            //Fix sticky bits
-//            NSString *partition_table_file = [NSString stringWithFormat:@"%@/%@-pt.vmdk", bootcamp_vm_dir, _vmdk_file_name];
-//            
-//            NSData *bootcamp_pt_data = [NSData dataWithContentsOfFile:partition_table_file];
-//            NSUInteger dataLength = [bootcamp_pt_data length];
-//            NSMutableString *string = [NSMutableString stringWithCapacity:dataLength*2];
-//            const unsigned char *dataBytes = [bootcamp_pt_data bytes];
-//            for (NSUInteger idx = 0; idx < dataLength; ++idx) {
-//                [string appendFormat:@"%02x", dataBytes[idx]];
-//            }
-//
-//            NSUInteger offset = 446; // byte of partition records start in MBR
-//            for (NSUInteger i = 0; i < [disk_s[1] intValue]-1; i++) {
-//                [string replaceCharactersInRange:NSMakeRange((offset + 16*i + 4)*2, 2) withString:@"2d"]; // change the fifth byte of each 16-bytes partition record before BOOTCAMP's partition record
-//            }
-//            
-//            NSMutableData *fixed_partition_table = [NSMutableData data];
-//            for (NSUInteger idx = 0; idx+2 <= string.length; idx+=2) {
-//                NSRange range = NSMakeRange(idx, 2);
-//                NSString *hexStr = [string substringWithRange:range];
-//                NSScanner *scanner = [NSScanner scannerWithString:hexStr];
-//                unsigned int intValue;
-//                [scanner scanHexInt:&intValue];
-//                [fixed_partition_table appendBytes:&intValue length:1];
-//            }
-//            [fixed_partition_table writeToFile:partition_table_file atomically:YES]; //TODO handle status
-//            
+            [create_vmdk_script executeAndReturnError:&errorDict];
+            [self appendTextToDetails:NSLocalizedString(@"DONE_MSG", "DONE_MSG")];
+
+            //Fix sticky bits
+            NSString *partition_table_file = [NSString stringWithFormat:@"%@/%@-pt.vmdk", bootcamp_vm_dir, _vmdk_file_name];
+            
+            NSData *bootcamp_pt_data = [NSData dataWithContentsOfFile:partition_table_file];
+            NSUInteger dataLength = [bootcamp_pt_data length];
+            NSMutableString *string = [NSMutableString stringWithCapacity:dataLength*2];
+            const unsigned char *dataBytes = [bootcamp_pt_data bytes];
+            for (NSUInteger idx = 0; idx < dataLength; ++idx) {
+                [string appendFormat:@"%02x", dataBytes[idx]];
+            }
+
+            NSUInteger offset = 446; // byte of partition records start in MBR
+            for (NSUInteger i = 0; i < [disk_s[1] intValue]-1; i++) {
+                [string replaceCharactersInRange:NSMakeRange((offset + 16*i + 4)*2, 2) withString:@"2d"]; // change the fifth byte of each 16-bytes partition record before BOOTCAMP's partition record
+            }
+            
+            NSMutableData *fixed_partition_table = [NSMutableData data];
+            for (NSUInteger idx = 0; idx+2 <= string.length; idx+=2) {
+                NSRange range = NSMakeRange(idx, 2);
+                NSString *hexStr = [string substringWithRange:range];
+                NSScanner *scanner = [NSScanner scannerWithString:hexStr];
+                unsigned int intValue;
+                [scanner scanHexInt:&intValue];
+                [fixed_partition_table appendBytes:&intValue length:1];
+            }
+            [fixed_partition_table writeToFile:partition_table_file atomically:YES]; //TODO handle status
+            
             // create a new VM
-            NSString *vbm_createvm = [NSString stringWithFormat:@"VBoxManage createvm --name \"%@\" --ostype \"WindowsXP\" --register --basefolder %@;", _vm_name, bootcamp_vm_dir];
-            NSString *vbm_modifyvm = [NSString stringWithFormat:@"VBoxManage modifyvm \"%@\" --memory 1024 --vram 64 --ioapic on --hwvirtex on --chipset piix3 --nestedpaging on --boot1 disk --boot2 none --boot3 none --boot4 none --rtcuseutc on --clipboard bidirectional --accelerate2dvideo on --audio coreaudio --audiocontroller ac97;", _vm_name];
-            NSString *vbm_storagectl = [NSString stringWithFormat:@"VBoxManage storagectl \"%@\" --name \"IDE Controller\" --add ide --controller ICH6;", _vm_name];
-            NSString *vbm_storageattach_hd = [NSString stringWithFormat:@"VBoxManage storageattach \"%@\" --storagectl \"IDE Controller\" --port 0 --device 0 --type hdd --medium %@/%@.vmdk;", _vm_name, bootcamp_vm_dir, _vmdk_file_name];
-            NSString *vbm_storageattach_addons = [NSString stringWithFormat:@"VBoxManage storageattach \"%@\" --storagectl \"IDE Controller\" --port 1 --device 0 --type dvddrive --medium /Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso", _vm_name];
+            NSString *vbm_createvm = [NSString stringWithFormat:@"VBoxManage createvm --name \\\"%@\\\" --ostype \\\"WindowsXP\\\" --register --basefolder %@;", _vm_name, bootcamp_vm_dir];
+            NSString *vbm_modifyvm = [NSString stringWithFormat:@"VBoxManage modifyvm \\\"%@\\\" --memory 1024 --vram 64 --ioapic on --hwvirtex on --chipset piix3 --nestedpaging on --boot1 disk --boot2 none --boot3 none --boot4 none --rtcuseutc on --clipboard bidirectional --accelerate2dvideo on --audio coreaudio --audiocontroller ac97;", _vm_name];
+            NSString *vbm_storagectl = [NSString stringWithFormat:@"VBoxManage storagectl \\\"%@\\\" --name \\\"IDE Controller\\\" --add ide --controller ICH6;", _vm_name];
+            NSString *vbm_storageattach_hd = [NSString stringWithFormat:@"VBoxManage storageattach \\\"%@\\\" --storagectl \\\"IDE Controller\\\" --port 0 --device 0 --type hdd --medium %@/%@.vmdk;", _vm_name, bootcamp_vm_dir, _vmdk_file_name];
+            NSString *vbm_storageattach_addons = [NSString stringWithFormat:@"VBoxManage storageattach \\\"%@\\\" --storagectl \\\"IDE Controller\\\" --port 1 --device 0 --type dvddrive --medium /Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso", _vm_name];
             
             //Assemble AppleScript
             NSMutableString *create_vm_source = [[NSMutableString alloc] init];
@@ -216,7 +216,6 @@
             //Execute AppleScript
             NSAppleScript *create_vm_script = [[NSAppleScript alloc] initWithSource:create_vm_source];
             [create_vm_script executeAndReturnError:&errorDict];
-
                 
         }
     }
